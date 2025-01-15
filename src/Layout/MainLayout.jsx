ed05389c62a984837b8db865b3eb32907edc874a
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-01-15 16:20:31 +0600

    Navbar and Footer section done.

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="bg-white">
+      <Navbar></Navbar>
+      <div className="pt-24 min-h-[calc(100vh-68px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/Router/AdminRoute.jsx
@@ -0,0 +1,13 @@
+// import { Navigate } from "react-router-dom";
+// import LoadingSpinner from "../components/Shared/LoadingSpinner";
+// import UseRole from "../hooks/UseRole";
+
+// const AdminRoute = ({ children }) => {
+//   const [role, isLoading] = UseRole();
+
+//   if (isLoading) return <LoadingSpinner />;
+//   if (role === "admin") return children;
+//   return <Navigate to="/dashboard" replace="true" />;
+// };
+
+// export default AdminRoute;
